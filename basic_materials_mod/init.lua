542b867d29ef8e31e31ce0862e5f99eb3b4cbce3
1F616EMO <root@1f616emo.xyz>
2024-01-06 13:23:01 +0800

    basic_materials_mod

---
+++ basic_materials_mod/init.lua
@@ -0,0 +1,20 @@
+-- twi_mods/basic_materials_mod/init.lua
+-- Basic Materials Modifications
+--[[
+    Copyright (C) 2024  1F616EMO
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+
+minetest.register_alias_force("basic_materials:padlock", "default:steel_ingot")
