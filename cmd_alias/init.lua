bef4ee8edd961ccf64f2216e14f0984eca2d4072
1F616EMO <root@1f616emo.xyz>
2024-02-12 22:39:11 +0800

    cmd_alias

---
+++ cmd_alias/init.lua
@@ -0,0 +1,44 @@
+-- twi_mods/cmd_alias/init.lua
+-- Register command alias
+--[[
+    Copyright © 2024 1F616EMO
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+]]
+
+local S = minetest.get_translator("cmd_alias")
+
+local cmd_alias = {
+    tp = "teleport",
+    doc = "helpform",
+    snp = "snippets",
+}
+
+local function do_alias(from, to)
+    if minetest.registered_chatcommands[to] then
+        local def = table.copy(minetest.registered_chatcommands[to])
+        def.description = S("Alias of /@1: @2", to, def.description or "")
+
+        minetest.register_chatcommand(from, def)
+    end
+end
+
+for from, to in pairs(cmd_alias) do
+    do_alias(from, to)
+end
