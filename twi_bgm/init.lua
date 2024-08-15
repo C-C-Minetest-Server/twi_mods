474b5b06ee8b71017c15d35808ee295cbad67eed
1F616EMO <root@1f616emo.xyz>
2024-08-15 19:25:27 +0800

    Tidy up BGMs

--- twi_bgm/init.lua
+++ twi_bgm/init.lua
@@ -43,6 +43,23 @@ background_music.register_music("twi_bgm:spawn_day", {
         gain = 0.4,
         resend_time = 97.5,
     },
+
+    -- Yuri, the Travelling Shoppe
+    -- Soundworlds Histories: Duskfire (John Oestmann)
+    -- License: CC0
+    {
+        name = "john_duskfire_07",
+        gain = 0.4,
+        resend_time = 100.2,
+    },
+
+    -- Little Town [TODO]
+    -- bart (remix: 1F616EMO)
+    -- https://opengameart.org/content/little-town
+    -- License: CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/
+    -- {
+    --     name = "1f616emo_little_town",
+    -- },
 })
 
 -- Songs played around Spawnpoint at night
@@ -75,50 +92,7 @@ background_music.register_music("twi_bgm:spawn_night", {
         gain = 0.4,
         resend_time = 62.3,
     },
-})
 
--- Songs played in SmushyVille in daytime
-background_music.register_music("twi_bgm:smushyville_day", {
-    -- Little Town [TODO]
-    -- bart (remix: 1F616EMO)
-    -- https://opengameart.org/content/little-town
-    -- License: CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/
-    -- {
-    --     name = "1f616emo_little_town",
-    -- },
-
-    -- Setpping Stones (No Drums)
-    -- Matthew Pablo
-    -- https://opengameart.org/content/stepping-stones
-    -- License: CC BY 3.0 https://creativecommons.org/licenses/by/3.0/
-    {
-        name = "matthew_stepping_stones_nodrums",
-        gain = 0.4,
-        resend_time = 102.9,
-    },
-
-    -- Woodland Fantasy
-    -- Matthew Pablo
-    -- https://opengameart.org/content/woodland-fantasy
-    -- License: CC BY 3.0 https://creativecommons.org/licenses/by/3.0/
-    {
-        name = "matthew_woodland_fantasy",
-        gain = 0.4,
-        resend_time = 150.1,
-    },
-
-    -- Yuri, the Travelling Shoppe
-    -- Soundworlds Histories: Duskfire (John Oestmann)
-    -- License: CC0
-    {
-        name = "john_duskfire_07",
-        gain = 0.4,
-        resend_time = 100.2,
-    },
-})
-
--- Songs played in SmushyVille at night
-background_music.register_music("twi_bgm:smushyville_night", {
     -- Deadlands Forest Shrine
     -- Soundworlds Datapedia: Ardune Ambient (John Oestmann)
     -- License: CC0
@@ -126,16 +100,6 @@ background_music.register_music("twi_bgm:smushyville_night", {
         name = "john_ardune_07",
         gain = 0.4,
         resend_time = 193.5,
-    },
-
-    -- A New Town (RPG Theme)
-    -- Pixelsphere OST (cynicmusic)
-    -- https://pixelsphere.org/
-    -- License: CC0
-    {
-        name = "pixelsphere_25_new_town",
-        gain = 0.4,
-        resend_time = 62.3,
     },
 })
 
@@ -148,8 +112,5 @@ background_music.register_on_decide_music(function(player)
     if func_areas.is_in_func_area(ppos, 497) then
         -- Spawn Island
         return now_day and "twi_bgm:spawn_day" or "twi_bgm:spawn_night"
-    -- elseif func_areas.is_in_func_area(ppos, 498) then
-    --     -- SmushyVille
-    --     return now_day and "twi_bgm:smushyville_day" or "twi_bgm:smushyville_night"
     end
 end)
