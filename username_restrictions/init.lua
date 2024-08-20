94ddffba2bd53f4af0335ccfa1dd8489fd0a3c57
1F616EMO <root@1f616emo.xyz>
2024-08-20 08:13:28 +0800

    Ban usernames containing "admin" or "moderator"

---
+++ username_restrictions/init.lua
@@ -0,0 +1,27 @@
+-- twi_mods/username_restrictions/init.lua
+-- Ban some usernames
+-- Copyright (C) 2024  1F616EMO
+-- SPDX-License-Identifier: LGPL-2.1-or-later
+
+local banned_parts = {}
+
+for _, part in ipairs({"admin", "moderator"}) do
+    banned_parts[part] = antispoof.normalize(part)
+end
+
+local auth
+minetest.register_on_prejoinplayer(function(name)
+    if name == "singleplayer" or name == minetest.settings:get("name") then return end
+    auth = auth or minetest.get_auth_handler()
+    if not auth.get_auth(name) then
+        local normalized_name = antispoof.normalize(name)
+        for orig_part, norm_part in pairs(banned_parts) do
+            if string.find(normalized_name, norm_part) then
+                return string.format(
+                    "Invalid username %s: Username cannt conain \"%s\"",
+                    name, orig_part
+                )
+            end
+        end
+    end
+end)
