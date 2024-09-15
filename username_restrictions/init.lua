b2851e36bc3f00d1c78704c876f6ab319082f4cb
1F616EMO <root@1f616emo.xyz>
2024-09-16 07:19:49 +0800

    ban L+O+L+

--- username_restrictions/init.lua
+++ username_restrictions/init.lua
@@ -14,6 +14,11 @@ minetest.register_on_prejoinplayer(function(name)
     if name == "singleplayer" or name == minetest.settings:get("name") then return end
     auth = auth or minetest.get_auth_handler()
     if not auth.get_auth(name) then
+        if string.find(name, "L+O+L+") then
+            -- As unclear as possible yet give some useful message
+            return "This username is banned."
+        end
+
         local normalized_name = antispoof.normalize(name)
         for orig_part, norm_part in pairs(banned_parts) do
             if string.find(normalized_name, norm_part) then
