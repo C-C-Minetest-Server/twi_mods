941497a6a9648219a4fa79cd0fc715995a0ec85e
1F616EMO <root@1f616emo.xyz>
2024-10-04 21:05:05 +0800

    Increase max speed of E231 series to 35 m/s

---
+++ advtrains_train_jre231_mod/init.lua
@@ -0,0 +1,13 @@
+-- twi_mods/advtrains_train_jre231_mod/init.lua
+-- Modify advtrains_train_jre231
+-- Copyright (C) 2024  1F616EMO
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+for _, name in ipairs({
+    "KuHa_E231",
+    "MoHa_E231",
+    "SaHa_E231",
+    "MoHa_E230",
+}) do
+    advtrains.wagon_prototypes["advtrains:" .. name].max_speed = 35
+end
