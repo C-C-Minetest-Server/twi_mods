5eabbdac1fe31748d7c987ac990678f0db840511
1F616EMO <root@1f616emo.xyz>
2025-01-28 12:52:16 +0800

    Fix quiver on players showing wielded item image

---
+++ skin_bugfix/init.lua
@@ -0,0 +1,20 @@
+-- twi_mods/skin_bugfix/init.lua
+-- Fix missing textures in player skins
+-- SPDX-License-Identifier: LGPL-3.0-or-later
+
+local expected_length = 5
+
+modlib.minetest.register_globalstep(5, function()
+    for _, player in ipairs(core.get_connected_players()) do
+        local skin = player:get_properties().textures
+
+        if #skin < expected_length then
+            for i = #skin + 1, expected_length do
+                skin[i] = "blank.png"
+            end
+
+            core.log("Fixing missing textures in player skin for player " .. player:get_player_name())
+            player:set_properties({ textures = skin })
+        end
+    end
+end)
