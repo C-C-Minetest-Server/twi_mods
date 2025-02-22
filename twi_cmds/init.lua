1a7a72aae664bbc278962ef8d27d6e511a762565
1F616EMO <root@1f616emo.xyz>
2025-02-22 08:20:00 +0800

    Add /night and introduce a universal 1-minute cooldown

--- twi_cmds/init.lua
+++ twi_cmds/init.lua
@@ -5,24 +5,47 @@
 
 local S = core.get_translator("twi_cmds")
 
+local cooldowns_expire = {}
+
+function do_settime_cmd(name, time_desc, time_value, time_sound)
+    local now = os.time()
+    if cooldowns_expire[name] and now < cooldowns_expire[name] then
+        return false, S("Be patient! Try this command again in @1 seconds.", cooldowns_expire[name] - now)
+    end
+
+    local current_time = core.get_timeofday()
+    if math.abs(current_time - time_value) < 0.042 then
+        return false, S("Be patient!")
+    end
+
+    cooldowns_expire[name] = now + 60
+
+    core.set_timeofday(time_value)
+    core.sound_play(time_sound, { gain = 1.0 })
+    core.log("action", name .. " set time to " .. time_desc)
+
+    for _, player in ipairs(core.get_connected_players()) do
+        local name = player:get_player_name()
+        background_music.set_start_play_gap(name, 2)
+        background_music.decide_and_play(player, true)
+    end
+
+    return true, S("Successfully set time to " .. time_desc .. ".")
+end
+
 core.register_chatcommand("day", {
     privs = { basic_settime = true },
     description = S("Set time to day"),
     func = function(name)
-        local current_time = core.get_timeofday()
-        local new_time = 0.3125
-        if math.abs(current_time - new_time) < 0.042 then
-            return false, S("Be patient!")
-        end
-        core.set_timeofday(new_time)
-        core.log("action", name .. " set time to day")
-        for _, player in ipairs(core.get_connected_players()) do
-            local name = player:get_player_name()
-            background_music.set_start_play_gap(name, 2)
-            background_music.decide_and_play(player, true)
-        end
-        core.sound_play("ui_morning", { gain = 1.0 })
-        return true, S("Successfully set time to day.")
+        return do_settime_cmd(name, "day", 0.3125, "ui_morning")
+    end,
+})
+
+core.register_chatcommand("night", {
+    privs = { basic_settime = true },
+    description = S("Set time to night"),
+    func = function(name)
+        return do_settime_cmd(name, "night", 0.8958, "ui_owl")
     end,
 })
 
@@ -50,4 +73,13 @@ core.register_on_joinplayer(function(player)
         core.set_player_privs(name, privs)
     end
     meta:set_int("twi_cmds_basic_settime", 1)
+end)
+
+modlib.minetest.register_globalstep(60, function()
+    local now = os.time()
+    for name, expire in pairs(cooldowns_expire) do
+        if now >= expire or not core.get_player_by_name(name) then
+            cooldowns_expire[name] = nil
+        end
+    end
 end)
