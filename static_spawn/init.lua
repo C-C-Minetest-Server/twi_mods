ffe8e0614d36f326df3a0c51c870a0c140dc9b90
1F616EMO <root@1f616emo.xyz>
2025-04-11 22:36:11 +0800

    Allow players to opt-in to the new spawn
    This does not (yet) affect static_spawnpoint, i.e. you always respawn at the old spawn if you haven't overriden your spawnpoint with a bed.

--- static_spawn/init.lua
+++ static_spawn/init.lua
@@ -21,6 +21,11 @@
 
 local S = core.get_translator("spawn")
 
+local POS_ORIGIN = { x = 37, y = 18, z = -42 }
+local POS_GRAPEHILLS = { x = -3292, y = 19, z = 1027 }
+
+-- Dynamic, teleport by static_spawn
+--[[
 core.register_chatcommand("spawn", {
     description = S("Teleport to the spawn point"),
     privs = {
@@ -40,6 +45,50 @@ core.register_chatcommand("spawn", {
         background_music.set_start_play_gap(name, 2)
         background_music.decide_and_play(player, true)
         return true, S("Teleported to Spawn!")
+    end
+})
+]]
+
+-- opt-in choice
+core.register_chatcommand("spawn", {
+    description = S("Teleport to the spawn point, or opt-in to the new spawn"),
+    params = "[set grapehills|set origin]",
+    privs = {
+        home = true,
+    },
+    func = function(name, params)
+        local player = core.get_player_by_name(name)
+        if not player then
+            return false, S("Player object not found.")
+        end
+
+        local meta = player:get_meta()
+        local spawn_choice = meta:get_string("static_spawn_choice")
+
+        if params ~= "" then
+            if params == "set grapehills" then
+                meta:set_string("static_spawn_choice", "grapehills")
+                return true, S("Spawn choice set to @1. Run /spawn again to teleport.", S("Grape Hills"))
+            elseif params == "set origin" then
+                meta:set_string("static_spawn_choice", "origin")
+                return true, S("Spawn choice set to @1. Run /spawn again to teleport.", S("Origin"))
+            else
+                return false, S("Invalid parameter. Only the following are allowed: @1.",
+                    "set grapehills / set origin")
+            end
+        end
+
+        local spawn_pos = POS_ORIGIN
+        local spawn_desc = S("Origin")
+        if spawn_choice == "grapehills" then
+            spawn_pos = POS_GRAPEHILLS
+            spawn_desc = S("Grape Hills")
+        end
+
+        player:set_pos(spawn_pos)
+        background_music.set_start_play_gap(name, 2)
+        background_music.decide_and_play(player, true)
+        return true, S("Teleported to @1!", spawn_desc)
     end
 })
 
@@ -84,7 +133,7 @@ core.register_chatcommand("origin", {
         player:set_pos({ x = 37, y = 18, z = -42 })
         background_music.set_start_play_gap(name, 2)
         background_music.decide_and_play(player, true)
-        return true, S("Teleported to Origin!")
+        return true, S("Teleported to @1!", S("Origin"))
     end
 })
 
@@ -102,6 +151,6 @@ core.register_chatcommand("grapehills", {
         player:set_pos({ x = -3292, y = 19, z = 1027 })
         background_music.set_start_play_gap(name, 2)
         background_music.decide_and_play(player, true)
-        return true, S("Teleported to Grape Hills!")
+        return true, S("Teleported to @1!", S("Grape Hills"))
     end
 })
