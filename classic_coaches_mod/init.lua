92ee30ea3ce2095f81d34d2ee845245dd5f8b0f9
1F616EMO <root@1f616emo.xyz>
2025-05-26 12:18:15 +0800

    Increase max speed of Advtrains classic and bistro coaches

---
+++ classic_coaches_mod/init.lua
@@ -0,0 +1,15 @@
+-- twi_mods/moretrains_mod_mod/init.lua
+-- Modify moretrains_mod
+-- Copyright (C) 2024  1F616EMO
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+for _, name in ipairs({
+    "classic_coaches:corridor_coach_class1",
+    "classic_coaches:corridor_coach_class2",
+    "classic_coaches:open_coach_class1",
+    "classic_coaches:open_coach_class2",
+    "classic_coaches:bistro_coach",
+}) do
+    core.registered_entities[name].max_speed = twi_fx.ADVTRAINS_MAX_TRAIN_SPEED
+    advtrains.wagon_prototypes[name].max_speed = twi_fx.ADVTRAINS_MAX_TRAIN_SPEED
+end
