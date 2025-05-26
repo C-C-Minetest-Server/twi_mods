e9096fb38243ec3a5cf07abbc9d9957ad179d1b6
1F616EMO <root@1f616emo.xyz>
2025-05-26 13:22:54 +0800

    Disable the chaotic /tpe

---
+++ tpr_mod/init.lua
@@ -0,0 +1,7 @@
+-- twi_mods/tpr_mod/init.lua
+-- Modify tpr
+-- Copyright (c)  2025 1F616EMO
+-- SPDX-License-Identifier: LGPL-2.1-or-later
+
+-- 1. Disable the chaotic /tpe
+core.registered_chatcommands["tpe"] = nil
