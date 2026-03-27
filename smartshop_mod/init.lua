742fb0cb9bbb18f160e125129db4da67a23267d5
1F616EMO <root@1f616emo.xyz>
2026-03-27 14:13:28 +0800

    Allow item injectors to access smartshop external storages

--- smartshop_mod/init.lua
+++ smartshop_mod/init.lua
@@ -6,7 +6,7 @@
 -- Do not use unlimited inventory unless in creative mode
 local old_initialize_metadata = smartshop.shop_class.initialize_metadata
 function smartshop.shop_class:initialize_metadata(player)
-    old_initialize_metadata(self, player)
+	old_initialize_metadata(self, player)
 
 	local player_name
 	if type(player) == "string" then
@@ -15,7 +15,29 @@ function smartshop.shop_class:initialize_metadata(player)
 		player_name = player:get_player_name()
 	end
 
-    if not core.is_creative_enabled(player_name) then
-        self:set_unlimited(false)
-    end
+	if not core.is_creative_enabled(player_name) then
+		self:set_unlimited(false)
+	end
+end
+
+-- Handle player names early, as futil is not happy with fake players
+-- But that's essensial on using item injectors
+-- Not gonna override futil.is_player as god knows where else it is used.
+local old_is_owner = smartshop.node_class.is_owner
+function smartshop.node_class:is_owner(player)
+	local player_name
+	if type(player) == "string" then
+		player_name = player
+	elseif type(player.get_player_name) == "function" then
+		local func_player_name = player:get_player_name()
+		if type(func_player_name) == "string" then
+			player_name = func_player_name
+		end
+	end
+
+	if type(player_name) ~= "string" then
+		return false
+	end
+
+	return old_is_owner(self, player_name)
 end
